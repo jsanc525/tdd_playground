fec835c4c4365b5f335a0c497279499fd0015c91
joe azerof <joe.sanchez5525@gmail.com>
2024-11-19 22:18:03 +0100

    fix(tests): updates function name in test

--- test_hashtable.py
+++ test_hashtable.py
@@ -1,9 +1,9 @@
 # test_hashtable.py
 
-from calculator import add_two_numbers
+from calculator import add_two_nums
 
 def test_should_always_pass():
     assert 2 + 2 == 4, "This is just a dummy test"
 
-def test_add_two_numbers():
-    assert add_two_numbers(2,2) == 4
+def test_add_two_nums():
+    assert add_two_nums(2,2) == 4
